0c814d758bd49612b597cd6ac449d6f22ed527f7
farzane nazmabadi <1379farzanenazmabadi@gmail.com>
2025-04-07 19:22:35 +0330

    first configuration

--- daily-status.php
+++ daily-status.php
@@ -3,15 +3,30 @@
  * Plugin Name:       Daily Status
  * Plugin URI:        https://example.com/plugins/the-basics/
  * Description:       Handle the basics with this plugin.
- * Version:           1.10.3
+ * Version:           1.0.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
- * Author:            John Smith
- * Author URI:        https://author.example.com/
+ * Author:            Farzane Nazmabadi
+ * Author URI:        https://farzanenazmabadi.ir/
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Update URI:        https://example.com/my-plugin/
- * Text Domain:       my-basics-plugin
+ * Text Domain:       daily-status
  * Domain Path:       /languages
- * Requires Plugins:  my-plugin, yet-another-plugin
  */
+
+
+ function daily_status_load_textdomain() {
+    load_plugin_textdomain('daily-status', false, dirname(plugin_basename(__FILE__)) . '/languages');
+}
+add_action('plugins_loaded', 'daily_status_load_textdomain');
+
+
+ define("DAILY_STATUS_DIR_PATH",plugin_dir_path( __FILE__ ));
+
+
+ if ( !defined( 'ABSPATH' ) ) {
+    exit;
+};
+
+require_once DAILY_STATUS_DIR_PATH."include/admin.php";
